13507811405d61f65d53c9105b2c11cd7936cc10
mustang2247 <mustang2247@gmail.com>
2017-07-20 22:42:07 +0800

    add utils

---
+++ hello-world/src/main/java/storm/cookbook/utils/DataSet.java
@@ -0,0 +1,23 @@
+package storm.cookbook.utils;
+
+public class DataSet {
+	private int times;
+	private int count;
+
+	public int getTimes() {
+		return times;
+	}
+
+	public void setTimes(int times) {
+		this.times = times;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+}
